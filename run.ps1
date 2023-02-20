74491e17f631a6eb824632a39659e276d4f2c0e6
BiscuitsLayer <vinogradov.so@phystech.edu>
2023-02-21 00:26:59 +0300

    Submodules added

--- run.ps1
+++ run.ps1
@@ -1,5 +1,14 @@
+# Build OOGL submodule .a (library) file
+Set-Location include/OOGL
+make
+Set-Location ../..
+
+# Build smartcar
 New-Item ./build -ItemType Directory
 Set-Location build
 cmake .. -G "MinGW Makefiles"
 make
-./SmartCarMain.exe  
+
+# Run smartcar
+./SmartCarMain.exe
+Set-Location ..
