d1b01247bbea2f34edd67e0488453a331e7925a8
BiscuitsLayer <vinogradov.so@phystech.edu>
2023-03-15 16:41:08 +0300

    [BUGFIX] Car wheels FPS drop fixed

--- src/car/car.hpp
+++ src/car/car.hpp
@@ -13,11 +13,18 @@ class CarModel : public Model {
 public:
     CarModel(std::string default_shader_name, std::string bbox_shader_name, std::string gltf, std::vector<std::string> car_wheel_mesh_names,
         float car_move_speed, float car_rotate_speed, float car_wheels_rotate_speed, GL::Vec3 car_center_translation, Transform transform)
-    : Model(default_shader_name, bbox_shader_name, gltf, transform), 
-    car_wheel_meshes_names_(car_wheel_mesh_names),
+    : Model(default_shader_name, bbox_shader_name, gltf, transform),
     car_move_speed_(car_move_speed), car_rotate_speed_(car_rotate_speed),
     car_wheels_rotate_speed_(car_wheels_rotate_speed),
     car_center_translation_(GL::Mat4{}.Translate(car_center_translation)) {
+        for (auto&& car_wheel_mesh_name : car_wheel_mesh_names) {
+            auto found = std::find_if(meshes_.begin(), meshes_.end(), [car_wheel_mesh_name](App::Mesh mesh) {
+                return mesh.name_ == car_wheel_mesh_name;
+            });
+            if (found != meshes_.end()) {
+                car_wheel_meshes_indicies_.push_back(found - meshes_.begin());
+            }
+        }
     }
 
     CarModel(Config::CarModelConfig config)
@@ -61,14 +68,8 @@ public:
 
 private:
     void RotateWheels(float delta_time) {
-        // TODO: FPS drop x2!!! to be fixed (init wheels -> set wheels transform -> just multiply self transform matrix)
-        for (auto&& car_wheel_mesh_name : car_wheel_meshes_names_) {
-            auto found = std::find_if(meshes_.begin(), meshes_.end(), [car_wheel_mesh_name](App::Mesh mesh) {
-                return mesh.name_ == car_wheel_mesh_name;
-            });
-            if (found != meshes_.end()) {
-                found->self_transform_.UpdateRotation(car_wheels_rotate_speed_ * delta_time, GL::Vec3(1.0f, 0.0f, 0.0f));
-            }
+        for (auto index : car_wheel_meshes_indicies_) {
+            meshes_[index].self_transform_.UpdateRotation(car_wheels_rotate_speed_ * delta_time, GL::Vec3(1.0f, 0.0f, 0.0f));
         }
     }
 
@@ -79,7 +80,7 @@ private:
     GL::Mat4 car_center_translation_;
     GL::Mat4 movement_transform_;
 
-    std::vector<std::string> car_wheel_meshes_names_;
+    std::vector<size_t> car_wheel_meshes_indicies_;
 
     friend class Gui;
 };
